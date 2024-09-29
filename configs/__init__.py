0e216c385f342edc8417e2a014a963d37703b8d8
Namerlight <shadabchoudhury@gmail.com>
2024-09-29 11:10:32 -0400

    added configs (remove this later

---
+++ configs/__init__.py
@@ -0,0 +1,7 @@
+from .keys import org, api_key
+
+# The keys.py file looks like this. Nothing else. Just put your keys in there.
+"""
+org = "org-something420"
+api_key = "sk-proj-blahblahblah123"
+"""
---
+++ configs/keys.py
@@ -0,0 +1,2 @@
+org = "org-wrTTGOG6dTwedVFE0qplndVr"
+api_key = "sk-proj-g9ZSNIQHVG2laNO9IlfGT3BlbkFJhuvHVaUUd9gf83DR6Ba7"
